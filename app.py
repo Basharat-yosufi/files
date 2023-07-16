543f0c7561a392d68037be48f228c3a614d76969
basharat <basharatyousufi786@gmail.com>
2023-07-16 18:12:49 +0500

    new file

---
+++ app.py
@@ -0,0 +1,15 @@
+if 9 > 8 :
+    print("true")
+if 9 < 8 : 
+    print(false) 
+if 5 > 3 :
+    print ("true")
+if 10 < 12 :
+    print("true")
+    if 12<15:
+        print("true")
+if 11 == 11 :
+    print ("equle")
+if 20 == 40 :
+    print(" it is impassable ")
+    
